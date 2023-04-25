0890559226463af765653e7b028d0784c029885b
Joao-F3rnando <joao.goncalves0108@gmail.com>
2023-04-24 22:38:12 -0300

    send message

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function(req, res){
     res.header("Access-Control-Allow-Origin", "*")
     res.send("Olá!!!")
+})
+
+app.post("/dado", function(req, res)
+{
+    res.header("Access-Control-Allow-Origin", "*")
+    console.log(req.body)
+    console.log("Recebi um dado")
+    console.log(req.dado)
+    res.send("JSON Recebido!!!")
 })
 
 app.listen(port, () => {
